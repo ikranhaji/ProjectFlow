b1b1c71860b7d29d22cc343a199ff3720c624670
ikranhaji <ikranhaji4@gmail.com>
2023-03-06 19:52:15 -0600

    Feature 17 complete

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,10 +5,4 @@ from .models import Task
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = (
-            "name",
-            "start_date",
-            "due_date",
-            "project",
-            "assignee"
-        )
+        fields = ("name", "start_date", "due_date", "project", "assignee")
